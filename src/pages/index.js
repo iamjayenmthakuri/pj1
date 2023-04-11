d1297eb7462e63283625fbcf3146828dcd69759d
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-11 11:53:28 +0545

    build run page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import About from "./components/About/About";
-import Contact from "@/pages/components/Contact";
 
 const inter = Inter({ subsets: ["latin"] });
 
