ce46caf62b8af8f28a29f55e9022b7026a0a1009
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-11 13:02:03 +0545

    HEllo world

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import Link from "next/link";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import About from "./about";
-import Contact from "./contact";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <Navbar>
-      <About />
-      <Contact />
-    </Navbar>
+    <>
+      <Navbar />
+      <div className="flex h-screen">
+        <Head>
+          <title>Home | my Website </title>
+        </Head>
+        <div className="m-auto">
+          <h1 className="text-6xl font-bold text-center">Hello World</h1>
+        </div>
+      </div>
+    </>
   );
 }
